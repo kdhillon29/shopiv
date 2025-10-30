16edf51d59bbba05857a1291a8b7acf148a97a33
kan dhillon <kdhillon469@gmail.com>
2025-10-31 10:29:56 +1100

    add dynamic home page

---
+++ app/components/Product.vue
@@ -0,0 +1,81 @@
+<template>
+  <div :id="`productcomp${product.id}`">
+    <div
+      class="relative flex flex-col gap-2 px-1 h-[28rem] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+    >
+      <NuxtLink :to="`product-${product.id}`">
+        <NuxtPicture
+          class="aspect-square object-contain h-1/2 shadow-sm rounded-t-lg"
+          :src="`${product.image}`"
+          alt="product image"
+        />
+      </NuxtLink>
+      <!-- <div class="px-2"> -->
+      <NuxtLink :to="`product-${product.id}`">
+        <h5
+          class="text-sm mt-3 md:text-md text-center font-semibold truncate tracking-tight text-gray-700 dark:text-white"
+        >
+          {{ product.title }}
+        </h5>
+      </NuxtLink>
+      <NuxtLink :to="`product-${product.id}`">
+        <p class="mt-3 text-gray-500 dark:text-gray-400 text-sm line-clamp-2">
+          {{ product.description }}
+        </p>
+      </NuxtLink>
+      <div class="flex items-center mt-1 dark:text-gray-400">
+        <h5 class="">Rating:</h5>
+        <span class="text-xs font-semibold px-2.5 py-0.5 rounded">{{
+          getRandomRating()
+        }}</span>
+      </div>
+      <div class="flex items-center justify-between gap-1 mt-1">
+        <strong class="text-gray-600 dark:text-gray-200">Price:</strong>
+        <span class="text-sm md:text-md font-semibold dark:text-white"
+          >${{ product.price }}</span
+        >
+
+        <span
+          class="text-sm md:text-md font-semibold dark:text-white line-through"
+          >${{ product.price * 2 }}</span
+        >
+        <br />
+      </div>
+      <button
+        @click="addToCart(product)"
+        class="flex items-center justify-center gap-3 text-white bg-orange-600 dark:bg-green-600 hover:bg-orange-800 mt-auto mb-3 w-[80%] mx-auto px-2 py-3 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm"
+      >
+        <Icon name="uil:cart" size="20" class="text-white font-semibold" />
+
+        <span v-if="alreadyInCart(cart, product) && user">Item Added</span>
+        <span v-else>Add to Cart</span>
+      </button>
+    </div>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script setup>
+defineProps(["product"]);
+
+const user = useSupabaseUser();
+const cart = useCart();
+const alreadyInCart = (cartState, productToCheck) => {
+  return cartState.some((productInCart) => {
+    return productInCart.id === productToCheck.id;
+  });
+};
+
+const getRandomRating = () => {
+  return Math.floor(Math.random() * 5) + 1;
+};
+
+const addToCart = (product) => {
+  console.log("Adding to cart:", product);
+  if (user.value) {
+    cart.value.push(product);
+  } else {
+    alert("Log in to start adding products to cart");
+  }
+};
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 px-2 py-4 md:px-6"
+  >
+    <div v-if="products" v-for="product in products">
+      <Product :product="product" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: products } = await useFetch("/api/products");
+console.log("products fetched:", products);
+</script>

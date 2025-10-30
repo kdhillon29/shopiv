7fec65a3a9afd9a7f71eb9e80923e4d7d8344aff
kan dhillon <kdhillon469@gmail.com>
2025-10-31 01:14:59 +1100

    add navbar

---
+++ app/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <PublicNav v-if="!user" />
+    <UserNav v-else />
+  </div>
+</template>
+
+<script setup>
+const user = useSupabaseUser();
+// const user = false;
+</script>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col min-h-screen bg-white dark:bg-gray-800">
+    <!-- <ThemeToggle /> -->
+    <Header />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup></script>

164b3105a2daed7e71f0359161ca0d883c734835
kan dhillon <kdhillon469@gmail.com>
2025-11-02 22:45:44 +1100

    update drizzle index

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,13 +1,14 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import "dotenv/config";
 import postgres from "postgres";
-import { Pool } from "pg";
+// import { Pool } from "pg";
 
 import * as schema from "../db/schema";
 
-// const queryClient = postgres(process.env.DATABASE_URL || "", {
-//   prepare: false,
-// });
-const client = new Pool({
-  connectionString: process.env.DATABASE_URL,
+const client = postgres(process.env.DATABASE_URL || "", {
+  prepare: false,
 });
-export const db = drizzle(client, { schema });
+// const client = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+export const db = drizzle(process.env.DATABASE_URL!);
